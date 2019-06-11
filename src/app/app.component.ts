e8870123e4ee1b348bf1317283a1ca77a6c150ff
fuji.nguyen <fuji.nguyen@live.com>
2019-06-10 23:37:54 -0400

    mock up for HR IPMS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Okta Demo';
+  title = 'HR IPMS SSO';
 
   constructor(private oauthService: OAuthService) {
     this.oauthService.redirectUri = window.location.origin;
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,41 +3,8 @@ import { OAuthService } from 'angular-oauth2-oidc';
 import { OktaAuthWrapper } from '../shared';
 
 @Component({
-  template: `
-    <div *ngIf="givenName" class="col-12 mt-2">
-      <button (click)="logout()" class="btn btn-sm btn-outline-primary float-right">Logout</button>
-      <h2>Welcome, {{givenName}}!</h2>
-      <p><a routerLink="/search" routerLinkActive="active">Search</a></p>
-    </div>
-
-    <div class="card mt-2" *ngIf="!givenName">
-      <div class="card-body">
-        <h4 class="card-title">Login with Authorization Server</h4>
-        <button class="btn btn-primary" (click)="login()">Login</button>
-      </div>
-    </div>
-
-    <div class="card mt-2" *ngIf="!givenName">
-      <div class="card-body">
-        <h4 class="card-title">Login with Username/Password</h4>
-
-        <p class="alert alert-error" *ngIf="loginFailed">
-          Login wasn't successful.
-        </p>
-
-        <div class="form-group">
-          <label>Username</label>
-          <input class="form-control" [(ngModel)]="username">
-        </div>
-        <div class="form-group">
-          <label>Password</label>
-          <input class="form-control" type="password" [(ngModel)]="password">
-        </div>
-        <div class="form-group">
-          <button class="btn btn-primary" (click)="loginWithPassword()">Login</button>
-        </div>
-      </div>
-    </div>`
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   username;
